4421fe91cb4cf6f60e3b4edb4dee65b7c46807b6
Mary Smia <mary.e.sima@gmail.com>
2016-02-15 16:02:46 -0800

    adding key stroke functionality

--- js/games/marys_box/components/MarysBox.react.js
+++ js/games/marys_box/components/MarysBox.react.js
@@ -1,7 +1,84 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 
+function slideRow(inputArr, reverse){
+	var arr = inputArr.slice(0);
+	var tempArray = [];
+	if (reverse){
+	    arr.reverse();
+	}
+	for (var i = 0; i < arr.length; i++){
+	    if (arr[i] !== undefined) {
+			tempArray.push(arr[i]); 
+	    }
+	}
+	var nextArray = []
+	for (var i = 0; i < tempArray.length; i++){
+	    if (tempArray[i] === tempArray[i+1]){
+			nextArray.push(tempArray[i]*2);
+			i++;
+	    }
+	    else {
+			nextArray.push(tempArray[i])
+	    }
+	}
+	for (var i = nextArray.length; i < arr.length; i++){
+	    nextArray.push(undefined)
+	}
+	if (reverse){
+	    nextArray.reverse();
+	}
+	console.log(nextArray)
+	return nextArray;
+}
+
+function slide(data, reverse) {
+    var newArray = data.map(function(row){
+	    return slideRow(row, reverse);
+	})
+    return newArray; 
+}
+function addNum(data) {
+    for (var row = 0; row < data.length; row++){
+	    for (var column = 0; column < data[row].length; column++) {
+		    if (data[row][column] === undefined) {
+			    data[row][column] = 2;
+				return data;
+			}
+    	}
+	}
+    return data;
+}
+function rotate(data, reverse){
+    var arr = data;
+	console.table(arr);
+	console.log(arr.length);
+	console.log(arr[0].length);
+	var newArray = [[],[],[],[]]
+	for (var row = 0; row < arr.length; row++) {
+	    for (var column = 0; column < arr[row].length; column++) {
+		    
+		    var value = arr[arr.length - column - 1][row];
+			if (reverse) {
+			  value = arr[column][arr.length - row - 1];
+			}
+		    newArray[row][column] = value;
+		}
+	}
+	console.table(newArray);
+    return newArray;
+}
+
 class MarysBox extends Component {
+    constructor(props) {
+        super(props);
+		this.state = {
+		   data: [[undefined, 2, 2, undefined],
+		         [4, undefined, undefined, undefined],
+		         [undefined, undefined, undefined, undefined],
+		         [undefined, undefined, undefined, undefined]]
+		}    
+    }
     row(values) {
         return (
             <div
@@ -37,8 +114,34 @@ class MarysBox extends Component {
             margin: 10,
         }}
             >{value}</div>);
+    } 
+    onKeyDown(event) {
+        switch(event.keyCode) {
+			case 38: // UP
+			    this.setState({data: addNum(rotate(slide(rotate(this.state.data, true))))})
+				event.preventDefault();
+				break;
+			case 40: // DOWN
+			    this.setState({data: addNum(rotate(slide(rotate(this.state.data, true), true)))})
+				event.preventDefault();
+				break;
+			case 37: // LEFT
+			    console.log(event.keyCode);
+				this.setState({data: addNum(slide(this.state.data))});
+				event.preventDefault();
+				break;
+			case 39: // RIGHT
+				this.setState({
+					data: addNum(slide(this.state.data, true))});
+				event.preventDefault();
+				break;
+			default: 
+				return;
+		}
     }
-
+    componentDidMount() {
+        window.addEventListener("keydown", this.onKeyDown.bind(this));
+    }
     render() {
         return (
             <div
@@ -53,10 +156,10 @@ class MarysBox extends Component {
                     margin: 50
                 }}
             >
-            {this.row([2, 8, 4, 2])}
-            {this.row([2, 4, 8, undefined])}
-            {this.row([2, undefined, 8, undefined])}
-            {this.row([2, 4, undefined, 16])}
+            {this.row(this.state.data[0])}
+            {this.row(this.state.data[1])}
+            {this.row(this.state.data[2])}
+            {this.row(this.state.data[3])}
             </div>
         );
     }
